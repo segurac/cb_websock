0371dfc0644597254f581d6350946b70dce32db9
Carlos Segura <carlos.seguraperales@telefonica.com>
2020-07-24 00:21:44 +0200

    message to pick up restarts

--- scripts/launch_db_websocks_mulitlog.sh
+++ scripts/launch_db_websocks_mulitlog.sh
@@ -5,6 +5,7 @@ capture_multilog () {
    product=$1
    while true
     do
+        echo "(Re)Starting capture $1"
         ./cb_websock $product |  multilog s104857600 n1000 '!/bin/gzip'  ./events_$product 
         sleep 0.5
     done 
