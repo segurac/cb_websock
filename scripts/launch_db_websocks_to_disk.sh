6afa8330bab0add28084b12b80676df0d3d88ac9
Carlos Segura <carlos.seguraperales@telefonica.com>
2020-07-27 16:13:52 +0200

    storing to disk ordered by date

---
+++ scripts/launch_db_websocks_to_disk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+capture_multilog () {
+   product=$1
+   while true
+    do
+        echo "(Re)Starting capture $1"
+#         ./cb_websock $product |  multilog s104857600 n1000 '!/bin/gzip'  ./events_$product 
+        ./cb_websock $product |  python preprocess_stdin.py coinbaseDump $product 
+        sleep 0.5
+    done 
+}
+
+
+for product in $(cat products.txt)
+do
+	echo $product
+    capture_multilog $product &
+done
+
+
+wait
+
